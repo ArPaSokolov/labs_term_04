412fa285e25ac1f98f408e1f88ea2076f649f435
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-03-23 05:50:40 +0200

    Destroy session

--- WEB/lab_3/code/logout.php
+++ WEB/lab_3/code/logout.php
@@ -1,1 +1,12 @@
 <?php
+session_start();
+
+if (isset($_POST['logout'])) {
+    // Уничтожение всех данных сессии
+    session_unset();
+    session_destroy();
+}
+
+// Перенаправление на другую страницу
+header('Location: index.php');
+exit();
