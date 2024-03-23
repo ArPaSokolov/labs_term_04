d49e24a5677f3290be11c2ee03fd4546e709e5d5
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-03-23 05:50:50 +0200

    Store and display user data

--- WEB/lab_3/code/index.php
+++ WEB/lab_3/code/index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+// Получение сохраненных данных из сессии
+$surname = $_SESSION['surname'] ?? '';
+$name = $_SESSION['name'] ?? 'Unknown';
+$age = $_SESSION['age'] ?? '';
+?>
+
 <!doctype html>
 <html lang="ru">
 <head>
@@ -8,10 +17,14 @@
     <title>lab_3</title>
 </head>
 <body>
+    <p><?php echo "$surname $name $age"; ?></p>
+
     <h1>Сontents:</h1>
 
     <a href="regular_expressions.php"><h2>Regular expressions</h2></a>
 
     <a href="form.php"><h2>Form</h2></a>
+
+    <a href="session.php"><h2>Session</h2></a>
 </body>
 </html>
