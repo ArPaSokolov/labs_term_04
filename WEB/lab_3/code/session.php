cc46ef62919452e94dcf14f1f8b931c1e886a16c
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-03-23 19:54:51 +0200

    Store session data in an array

--- WEB/lab_3/code/session.php
+++ WEB/lab_3/code/session.php
@@ -1,16 +1,30 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Получение данных из формы
-    $surname = $_POST['surname'];
-    $name = $_POST['name'];
-    $age = $_POST['age'];
+$userData = $_SESSION['userdata'] ?? [];
 
-    // Сохранение данных в сессию
-    $_SESSION['surname'] = $surname;
-    $_SESSION['name'] = $name;
-    $_SESSION['age'] = $age;
+if ('POST' === $_SERVER['REQUEST_METHOD']) {
+    // получение данных из формы
+    // Task 2
+    $surname = ($_POST['surname'] ?? '');
+    $name = ($_POST['name'] ?? '');
+    $age = ($_POST['age'] ?? '');
+    // Task 3
+    $sex = ($_POST['sex'] ?? '');
+    $salary = ($_POST['salary'] ?? '');
+
+    // создание массива данных
+    // Task 3
+    $userData = [
+        'surname' => $surname,
+        'name' => $name,
+        'age' => $age,
+        'salary' => $salary,
+        'sex' => $sex
+    ];
+
+    // сохранение данных в сессию
+    $_SESSION['userdata'] = $userData; // массив данных
 
     // Перенаправление на другую страницу
     header("Location: index.php");
@@ -33,28 +47,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             padding: 5px;
             margin-bottom: 10px;
         }
-
     </style>
 </head>
 <body>
+<h1>Session</h1>
+
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
-    <h1>Session</h1>
     <h2>Task 2</h2>
 
     <label>Surname:</label>
-    <input type="text" name="surname" required><br>
+    <input type="text" name="surname" value="<?php echo $userData['surname'] ?? '*'; ?>" required><br>
 
     <label>Name:</label>
-    <input type="text" name="name" required><br>
+    <input type="text" name="name" value="<?php echo $userData['name'] ?? '*'; ?>" required><br>
 
     <label>Age:</label>
-    <input type="number" name="age" required><br>
+    <input type="text" name="age" value="<?php echo $userData['age'] ?? '*'; ?>" required><br>
 
-    <button type="submit">Save profile</button>
-</form>
+<h2>Task 3</h2>
+
+    <label>Sex:</label>
+    <input type="text" name="sex" value="<?php echo $userData['sex'] ?? ''; ?>"><br>
+
+    <label>Salary:</label>
+    <input type="text" name="salary" value="<?php echo $userData['salary'] ?? ''; ?>"><br>
+
+    <button type="submit">Save</button>
+</form><br>
 
 <form action="logout.php" method="post">
-    <button type="submit" name="logout">Exit profile</button>
+    <button type="submit" name="logout">Delete profile</button>
 </form>
 
 <a href="index.php"><h2>Back</h2></a>
