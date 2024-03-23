392e0e70f1f45fae86740b4ca7922e7df0d51d42
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-03-23 03:27:33 +0200

    Splitting into pages

--- WEB/lab_3/code/index.php
+++ WEB/lab_3/code/index.php
@@ -8,33 +8,8 @@
     <title>lab_3</title>
 </head>
 <body>
-    <h1>Regular expressions</h1>
-    <h2>Task 1</h2>
-    <?php
-    $pattern = '/a..b/'; // регулярка
-    $text = 'ahb acb aeb aeeb bdha aj ab adcb axeb acctb';
+    <h1>Сontents:</h1>
 
-    preg_match_all($pattern, $text, $matches);
-    echo "Строки подошедшие под шаблон:<br>";
-    foreach ($matches[0] as $match) {
-        echo $match."<br>";
-    }
-    ?>
-
-    <h2>Task 2</h2>
-    <?php
-    // Task 2
-    function cubing($matches): string // возводим в куб
-    {
-        return $matches[0] ** 3;
-    }
-
-    $pattern = '/(\d+)/'; // регулярка
-    $string = 'a1b2c3d4e5';
-
-    $stringWithCubes = preg_replace_callback($pattern, 'cubing', $string);
-
-    echo "Новая строка с кубами:<br>" . $stringWithCubes . "<br>";
-    ?>
+    <a href="regular_expressions.php"><h2>Regular expressions</h2></a>
 </body>
 </html>
