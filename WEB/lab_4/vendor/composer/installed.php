e4e6879356fe64feb1f8591236ed5fc070ed97a3
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-04-15 01:17:26 +0200

    Initialize Composer

---
+++ WEB/lab_4/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sesha/lab_4',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '040e8008d6569d398209c0cc1efcd416496e295e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sesha/lab_4' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '040e8008d6569d398209c0cc1efcd416496e295e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wixel/gump' => array(
+            'pretty_version' => 'v1.11.0',
+            'version' => '1.11.0.0',
+            'reference' => 'becdf2e7941ba56171063de81dd1c2100fa496f6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wixel/gump',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
