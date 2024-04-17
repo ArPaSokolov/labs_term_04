cacb4ccb4d74acef7ec0649b15df8cf1bf4a4f12
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-04-17 20:48:54 +0200

    Show announcements from Google Sheets

--- WEB/lab_4/code/board.php
+++ WEB/lab_4/code/board.php
@@ -29,5 +29,61 @@
 
     <button type="submit">Post</button>
 </form>
+</form>
+<div id="table">
+    <table>
+        <thead>
+        <tr>
+            <th>Email</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Description</th>
+        </tr>
+        </thead>
+        <tbody>
+            <?php
+            require __DIR__ . '/vendor/autoload.php'; // Подключаем Google API Client Library
+
+            $client = new \Google_Client();
+            $client->setApplicationName('The Bulletin Board');
+            $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+            $client->setAccessType('offline');
+            $path = __DIR__ . '/credentials.json';
+            $client->setAuthConfig($path);
+
+            // Конфигурируем Sheets Service
+            $service = new Google\Service\Sheets($client);
+
+            // Идентификатор таблицы
+            $spreadsheetId = '1jnexgBDAHJq3gzLwzfbyHRkSLQa2EyOMPogOjvwYk9M';
+
+            // Определение диапазона для записи
+            $range = 'Sheet1!A:D';
+
+            $service = new Google_Service_Sheets($client);
+            $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+            $values = $response->getValues();
+
+            // Отображение данных в виде таблицы
+            if (!empty($values)) {
+                $isFirstRow = true; // Флаг для определения первой строки
+                foreach ($values as $row) {
+                    if ($isFirstRow) {
+                        $isFirstRow = false;
+                        continue; // Пропускаем первую строку
+                    }
+                    echo '<tr>';
+                    foreach ($row as $cell) {
+                        echo '<td>' . htmlspecialchars($cell) . '</td>';
+                    }
+                    echo '</tr>';
+                }
+            } else {
+                echo 'No data available.';
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
